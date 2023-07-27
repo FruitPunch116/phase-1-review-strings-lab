deb7d0919457e5dedac8e5c1c5e132c9cd1c8da8
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-07-27 13:36:06 -0400

    test results

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// Write your code in this file!
 let currentUser = "Grace Hooper"
+//console.log(currentUser)  /* Testing process */
 
 const welcomeMessage = `Welcome to Flatbook, ` + `${currentUser}` + `!`;
 
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ` + `${currentUser.toUpperCase()}` + `!`;
+//console.log(currentUser.toUpperCase()) /* Testing process */
 
 const shortGreeting  = `Welcome, ` + `${currentUser[0]}` + `!`;
+//console.log(currentUser[0]) /* Testing process */
